4b662460e203beee3fe54efd642c8d4d94a01785
MsheesAI <shees7795@gmail.com>
2026-02-15 16:26:55 +0500

    quiz 1 completed

---
+++ main.py
@@ -0,0 +1,8 @@
+#Question 1 print only neagative numbers from the list
+numbers = [1, -2, -13, 4, -5, 6,-7,8,9,11]
+negative_numbers = [num for num in numbers if num < 0]
+print("Negative numbers in the list:", negative_numbers)
+
+
+#Question 2 : Multiplication table using while loop
+start = input("Enter starting point for the multiplication")
