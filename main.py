c667a78427246bffe2c8833c20abde5148de6b28
MsheesAI <shees7795@gmail.com>
2026-02-15 17:00:17 +0500

    completed

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ for i in range(start, end + 1):
     print(f"{start}X{i} = {i*start}")
 
 #question3 : Temperature checker program
+user_input = str(input("Enter your city name"))
+temperature_input = int((input("Enter your city temperature")))
+
+if temperature_input == 0:
+    print("Its freezing outside in {user_input}")
+elif temperature_input <0 and temperature_input>10:
+    print("Its too cold in{user_input}")
+elif temperature_input == 10 and temperature_input>20:
+    print("ts getting cold in{user_input}")
+elif temperature_input == 20 and temperature_input>30:
+    print("Its a pleasant weather in{user_input}")
+elif temperature_input == 30 and temperature_input>40:
+    print("Its getting hot in{user_input}")
+elif temperature_input == 40 and temperature_input>50:
+    print("Its too much hot outside in{user_input}")
+elif temperature_input < 50:
+    print("hahahhaha game over{user_input}")
+
+
+
+
+
+
+
 
 
 
